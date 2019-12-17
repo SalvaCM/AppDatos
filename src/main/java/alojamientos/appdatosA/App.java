b348112c8079b87c9d087d60feb8c9f8c703b3eb
IN1DM3B_09 <IN1DM3B_09@H015112.NET>
2019-12-17 08:58:30 +0100

    primero commito

---
+++ src/main/java/alojamientos/appdatosA/App.java
@@ -0,0 +1,13 @@
+package alojamientos.appdatosA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
