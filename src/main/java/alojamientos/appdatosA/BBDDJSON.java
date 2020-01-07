00c46a0d857939b1060c1f9e3c5d08171b2ed735
IN2DM3B_10 <IN2DM3B_10@H015112.NET>
2020-01-07 08:13:57 +0100

    cambio a ip de clase

--- src/main/java/alojamientos/appdatosA/BBDDJSON.java
+++ src/main/java/alojamientos/appdatosA/BBDDJSON.java
@@ -19,7 +19,7 @@ public class BBDDJSON {
 		try
 		{
 		   Class.forName("com.mysql.cj.jdbc.Driver");
-		   Connection conexion = DriverManager.getConnection ("jdbc:mysql://192.168.0.7/alojamientos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "");
+		   Connection conexion = DriverManager.getConnection ("jdbc:mysql://192.168.101.24/alojamientos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "");
 		   String sql = "SELECT * FROM treservas";
 		   PreparedStatement stmt = conexion.prepareStatement(sql);
 		   ResultSet rs = stmt.executeQuery();
@@ -55,7 +55,7 @@ public class BBDDJSON {
 		try
 		{
 		   Class.forName("com.mysql.cj.jdbc.Driver");
-		   Connection conexion = DriverManager.getConnection ("jdbc:mysql://192.168.0.7/alojamientos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "");
+		   Connection conexion = DriverManager.getConnection ("jdbc:mysql://192.168.101.24/alojamientos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "");
 		   String sql = "SELECT * FROM tusuarios";
 		   PreparedStatement stmt = conexion.prepareStatement(sql);
 		   ResultSet rs = stmt.executeQuery();
@@ -93,7 +93,7 @@ public class BBDDJSON {
 		try
 		{
 		   Class.forName("com.mysql.cj.jdbc.Driver");
-		   Connection conexion = DriverManager.getConnection ("jdbc:mysql://192.168.0.7/alojamientos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "");
+		   Connection conexion = DriverManager.getConnection ("jdbc:mysql://192.168.101.24/alojamientos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "");
 		   String sql = "SELECT * FROM talojamientos";
 		   PreparedStatement stmt = conexion.prepareStatement(sql);
 		   ResultSet rs = stmt.executeQuery();
